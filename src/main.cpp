40750e735808637eca61e6664afdcf0bdd6f59cf
thomas.every <thomas.every>
2018-01-25 10:12:28 +0000

    Flashing all three LED's at a fixed speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <mbed.h>
 
 DigitalOut blue(LED_BLUE);
+DigitalOut red(LED_RED);
+DigitalOut green(LED_GREEN);
 
 Ticker  pit;
 
@@ -9,12 +11,16 @@ void flash(void) {
 
    if(phase==0) blue.write(0); /* on */
    if(phase==1) blue.write(1); /* off */
+   if(phase==2) red.write(0); /* on */
+   if(phase==3) red.write(1); /* off */
+   if(phase==4) green.write(0); /*on */
+   if(phase==5) green.write(1); /* off */
 
    phase++;
-   if(phase==3) phase=0;
+   if(phase==6) phase=0;
 }
 
 int main(void) {
-  pit.attach(flash,0.5);
+  pit.attach(flash,0.2);
   while(1);
 }
